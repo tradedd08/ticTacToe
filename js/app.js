94d245bad05a3e721f3e064b1ec3d7f441894e3e
Student <student@GAs-MacBook-Air-3.local>
2017-04-11 21:15:07 -0400

    titactoe

---
+++ js/app.js
@@ -0,0 +1,189 @@
+let x = 'x';
+let o = 'o';
+let player = 'X';
+
+
+//button row 1 tile 1
+
+
+document.querySelector('#tile1').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile1.innerHTML === ""){
+    tile1.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile1.innerHTML === ""){
+        
+    tile1.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+
+
+
+
+
+//button tile 2
+
+
+document.querySelector('#tile2').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile2.innerHTML === ""){
+    tile2.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile2.innerHTML === ""){
+        
+    tile2.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+
+
+
+
+// button tile 3
+document.querySelector('#tile3').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile3.innerHTML === ""){
+    tile3.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile3.innerHTML === ""){
+        
+    tile3.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+//row 2 button 4 
+
+
+
+document.querySelector('#tile4').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile4.innerHTML === ""){
+    tile4.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile4.innerHTML === ""){
+        
+    tile4.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+//row 2 button 5 
+
+
+
+document.querySelector('#tile5').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile5.innerHTML === ""){
+    tile5.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile5.innerHTML === ""){
+        
+    tile5.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+//row 2 button 5 
+
+
+
+document.querySelector('#tile5').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile5.innerHTML === ""){
+    tile5.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile5.innerHTML === ""){
+        
+    tile5.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+//row 2 button 6 
+
+
+
+document.querySelector('#tile6').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile6.innerHTML === ""){
+    tile6.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile6.innerHTML === ""){
+        
+    tile6.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+
+//row 3 button 7 
+
+
+
+document.querySelector('#tile7').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile7.innerHTML === ""){
+    tile7.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile7.innerHTML === ""){
+        
+    tile7.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+//row 3 button 8 
+
+
+
+document.querySelector('#tile8').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile8.innerHTML === ""){
+    tile8.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile8.innerHTML === ""){
+        
+    tile8.innerHTML = player;
+    player = 'X';
+    } 
+
+});
+
+
+
+//row 3 button 9 
+
+
+
+document.querySelector('#tile9').addEventListener('click', function(){
+    console.log('I was pressed')
+    if (player === 'X' && tile9.innerHTML === ""){
+    tile9.innerHTML = player;
+    player = 'O';
+    } else if (player === 'O' && tile9.innerHTML === ""){
+        
+    tile9.innerHTML = player;
+    player = 'X';
+    } 
+
+});
